1d6bba21151d4466fbfe66b8e28171df30157c28
Henriquy <henriquy1224@gmail.com>
2025-06-04 14:36:03 -0300

    feat: :tada: início do projeto + configuração inicial da primeira tela

---
+++ src/app/_components/navbar/navbar.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent implements OnInit {
+  ngOnInit(): void {
+    console.log('Componente NavbarComponent inicializado');
+  }
+}
