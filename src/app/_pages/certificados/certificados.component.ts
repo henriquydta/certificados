0099703779a56dfb824013f8846322d34f6bcc13
Henriquy <henriquy1224@gmail.com>
2025-06-09 14:56:30 -0300

    feat: :art: adição de novas páginas e refac para o uso de variáveis para as cores do projeto

--- src/app/_pages/certificados/certificados.component.ts
+++ src/app/_pages/certificados/certificados.component.ts
@@ -1,9 +1,7 @@
 import { Component } from '@angular/core';
-import { SecondaryButtonComponent } from "../../_components/secondary-button/secondary-button.component";
 
 @Component({
   selector: 'app-certificados',
-  imports: [SecondaryButtonComponent],
   templateUrl: './certificados.component.html',
   styleUrl: './certificados.component.css'
 })
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "./_components/navbar/navbar.component";
-import { PrimaryButtonComponent } from "./_components/primary-button/primary-button.component";
-import { SecondaryButtonComponent } from "./_components/secondary-button/secondary-button.component";
-import { ItemCertificadoComponent } from "./_components/item-certificado/item-certificado.component";
 import { BaseUiComponent } from "./_components/base-ui/base-ui.component";
-import { CertificadosComponent } from "./_pages/certificados/certificados.component";
+import { CertificadoComponent } from "./_pages/certificado/certificado.component";
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent, PrimaryButtonComponent, SecondaryButtonComponent, ItemCertificadoComponent, BaseUiComponent, CertificadosComponent],
+  imports: [NavbarComponent, BaseUiComponent, CertificadoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
