9184d497c3d14334830f4df0fff23146769048f5
Henriquy <henriquy1224@gmail.com>
2025-06-12 15:33:46 -0300

    feat: :sparkles: correção de componentes + fim da estilização e estruturação html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "./_components/navbar/navbar.component";
 import { BaseUiComponent } from "./_components/base-ui/base-ui.component";
-import { CertificadoComponent } from "./_pages/certificado/certificado.component";
+import { CertificadoFormComponent } from "./_pages/certificado-form/certificado-form.component";
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent, BaseUiComponent, CertificadoComponent],
+  imports: [NavbarComponent, BaseUiComponent, CertificadoFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
