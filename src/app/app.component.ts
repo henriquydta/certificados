5324bfaedbe80d87f0cabe79bc5cffbaffd8853e
Henriquy <henriquy1224@gmail.com>
2025-06-13 21:39:12 -0300

    feat: :truck: adição de roteamento

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "./_components/navbar/navbar.component";
 import { BaseUiComponent } from "./_components/base-ui/base-ui.component";
-import { CertificadoFormComponent } from "./_pages/certificado-form/certificado-form.component";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent, BaseUiComponent, CertificadoFormComponent],
+  imports: [RouterOutlet, NavbarComponent, BaseUiComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { CertificadosComponent } from './_pages/certificados/certificados.component';
+import { CertificadoFormComponent } from './_pages/certificado-form/certificado-form.component';
+import { CertificadoComponent } from './_pages/certificado/certificado.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    component: CertificadosComponent
+  },
+  {
+    path: "certificados/gerar",
+    component: CertificadoFormComponent
+  },
+  {
+    path: "certificados/:id",
+    component: CertificadoComponent
+  }
+];
